e2bcfe6002798eb96876416f8b1e2720724b1ebf
Mitch Cairns <mitch.cairns@handheldlegend.com>
2026-07-28 21:03:48 -0700

    Match FUN_023345fc: UTF-16 / halfword string length helper

---
+++ src/arm9/FUN_023345fc.c
@@ -0,0 +1,8 @@
+// decomp: module=unk_autoload_0 addr=0x023345fc name=FUN_023345fc
+int FUN_023345fc(const unsigned short *p) {
+    int len = -1;
+    do {
+        len++;
+    } while (*p++ != 0);
+    return len;
+}
