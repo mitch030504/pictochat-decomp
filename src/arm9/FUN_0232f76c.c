e113a68eaa2917125b730dda2cb73c7e254d77be
Mitch Cairns <mitch.cairns@handheldlegend.com>
2026-07-28 20:51:45 -0700

    Match FUN_0232e178, FUN_0232e988, FUN_0232f76c

---
+++ src/arm9/FUN_0232f76c.c
@@ -0,0 +1,5 @@
+// decomp: module=unk_autoload_0 addr=0x0232f76c name=FUN_0232f76c
+void FUN_0232f76c(int a, int b) {
+    int *base = *(int **)(*(int *)0x023bf010 + 0x84);
+    *(int *)((char *)base + (a << 4) + 0x14) = b;
+}
