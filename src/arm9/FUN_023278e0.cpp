ff0b3b20cf3cf5a2de0ed7f2574d7d424f5549d4
Mitch Cairns <mitch.cairns@handheldlegend.com>
2026-08-04 17:56:17 -0700

    Replace asm function with pure C tail-call FUN_023278e0

---
+++ src/arm9/FUN_023278e0.cpp
@@ -0,0 +1,9 @@
+//cpp
+// decomp: module=unk_autoload_0 addr=0x023278e0 name=FUN_023278e0
+extern "C" {
+extern void FUN_0232e94c(char *, int);
+extern char G_0239fe38[];
+void FUN_023278e0(void) {
+    FUN_0232e94c(G_0239fe38, 0);
+}
+}
--- src/arm9/FUN_0232da88.cpp
+++
@@ -1,7 +0,0 @@
-//cpp
-// decomp: module=unk_autoload_0 addr=0x0232da88 name=FUN_0232da88
-#pragma thumb on
-extern "C" asm void FUN_0232da88(void) {
-    swi 0x18
-    bx lr
-}
